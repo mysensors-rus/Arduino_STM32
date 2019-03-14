740771a1dfd1985e84f533997809fb41c389d134
Dab0G <vladimir@cympak.biz>
2019-03-14 10:54:37 +0300

    Add mode upload ST-link with bootloader

---
+++ tools/win/stlink_upload_bootloader.bat
@@ -0,0 +1,18 @@
+@echo off
+rem: Note %~dp0 get path of this batch file
+rem: Need to change drive if My Documents is on a drive other than C:
+set driverLetter=%~dp0
+set driverLetter=%driverLetter:~0,2%
+%driverLetter%
+cd %~dp0
+rem: the two line below are needed to fix path issues with incorrect slashes before the bin file name
+set addr=%1
+set str=%2
+set str=%str:/=\%
+
+
+rem: ------------- use STLINK CLI
+stlink\ST-LINK_CLI.exe -c SWD -P %str% %addr% -Rst -Run
+
+rem: Using the open source texane-stlink instead of the proprietary STM stlink exe
+rem:texane-stlink\st-flash.exe write %str% 0x8001000
